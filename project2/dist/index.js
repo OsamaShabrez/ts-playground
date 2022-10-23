874623472ec06d4db5c5ea2798c3e03398230749
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-10-23 18:41:35 +0200

    feat(todo-list): very basic implementation of todo list

--- project2/dist/index.js
+++ project2/dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hello World!");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const form = document.querySelector("form");
+const ul = document.getElementById("list");
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.value === "")
+        return;
+    const newTodoText = input.value;
+    const newLi = document.createElement("li");
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newLi.append(newCheckbox);
+    newLi.append(newTodoText);
+    ul.append(newLi);
+    input.value = "";
+};
+form.addEventListener("submit", handleSubmit);
--- project2/src/index.ts
+++ project2/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const btn = document.getElementById("btn")!;
+const input = document.getElementById("input")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const ul = document.getElementById("list")!;
+
+const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    if (input.value === "")
+        return; 
+    const newTodoText = input.value;
+    const newLi = document.createElement("li");
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newLi.append(newCheckbox);
+    newLi.append(newTodoText);
+    ul.append(newLi);
+    input.value = "";
+};
+
+form.addEventListener("submit", handleSubmit);
