541bebb6c30736eb1c790918c076caa8eb99aa5f
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-10-23 19:50:01 +0200

    feat(Todo-List-Project): complete code for todo list project

--- project2/dist/index.js
+++ project2/dist/index.js
@@ -3,17 +3,42 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("input");
 const form = document.querySelector("form");
 const ul = document.getElementById("list");
+const readLocalStorage = () => {
+    const itemsText = localStorage.getItem("todos");
+    if (itemsText === null) {
+        return [];
+    }
+    return JSON.parse(itemsText);
+};
+const saveLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(items));
+};
+const createToDo = (item) => {
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.isDone;
+    checkbox.addEventListener("change", () => {
+        item.isDone = checkbox.checked;
+        saveLocalStorage();
+    });
+    listItem.append(checkbox);
+    listItem.append(item.text);
+    ul.append(listItem);
+};
+const items = readLocalStorage();
+items.forEach(createToDo);
 const handleSubmit = (e) => {
     e.preventDefault();
     if (input.value === "")
         return;
-    const newTodoText = input.value;
-    const newLi = document.createElement("li");
-    const newCheckbox = document.createElement("input");
-    newCheckbox.type = "checkbox";
-    newLi.append(newCheckbox);
-    newLi.append(newTodoText);
-    ul.append(newLi);
+    const newItem = {
+        text: input.value,
+        isDone: false
+    };
+    createToDo(newItem);
+    items.push(newItem);
+    saveLocalStorage();
     input.value = "";
 };
 form.addEventListener("submit", handleSubmit);
--- project2/src/index.ts
+++ project2/src/index.ts
@@ -1,19 +1,55 @@
+interface ToDo {
+    text: string,
+    isDone: boolean
+}
+
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("input")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const ul = document.getElementById("list")!;
 
+const readLocalStorage = (): ToDo[] => {
+    const itemsText = localStorage.getItem("todos");
+    if (itemsText === null) {
+        return [];
+    }
+    return JSON.parse(itemsText);
+};
+const saveLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(items));
+};
+
+const createToDo = (item: ToDo) => {
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.isDone;
+    checkbox.addEventListener("change", () => {
+        item.isDone = checkbox.checked;
+        saveLocalStorage();
+    });
+    listItem.append(checkbox);
+    listItem.append(item.text);
+    ul.append(listItem);
+};
+
+const items: ToDo[] = readLocalStorage();
+items.forEach(createToDo);
+
 const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
     if (input.value === "")
-        return; 
-    const newTodoText = input.value;
-    const newLi = document.createElement("li");
-    const newCheckbox = document.createElement("input");
-    newCheckbox.type = "checkbox";
-    newLi.append(newCheckbox);
-    newLi.append(newTodoText);
-    ul.append(newLi);
+        return;
+
+    const newItem: ToDo = {
+        text: input.value,
+        isDone: false
+    }
+    createToDo(newItem);
+    items.push(newItem);
+
+    saveLocalStorage();
+
     input.value = "";
 };
 
